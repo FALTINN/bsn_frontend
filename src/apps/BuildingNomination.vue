d4f7a09ba6232191e0d5f2f59fc102b039811255
Timurka <tima_abdrashitov.00@mail.ru>
2024-12-19 23:18:48 +0300

    html added

---
+++ src/apps/BuildingNomination.vue
@@ -0,0 +1,25 @@
+<style>
+        body {
+            margin: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+            background-color: #f0f0f0;
+        }
+        .svg-container {
+            width: 100%;
+            max-width: 1920px;
+            height: auto;
+            overflow: hidden;
+        }
+        svg {
+            display: block;
+            width: 100%;
+            height: auto;
+        }
+    </style>
+
+<template>
+    HELLO
+</template>
--- src/router.js
+++ src/router.js
@@ -1,21 +1,46 @@
 import {createWebHistory, createRouter } from "vue-router";
 
-import CatalogGame from "./apps/CatalogGame.vue";
-import StoreGame from "./apps/StoreGame.vue";
 import MainPage from "./apps/MainPage.vue";
+import RichNomination from "./apps/RichNomination.vue";
+import BuilderNomination from "./apps/BuilderNomination.vue";
+import KingNomination from "./apps/KingNomination.vue";
+import RedStonerNomination from "./apps/RedStonerNomination.vue";
+import CreatorNomination from "./apps/CreatorNomination.vue";
+import EventNomination from "./apps/EventNomination.vue";
+import BuildingNomination from "./apps/BuildingNomination.vue";
 
 const routes = [
     {
-        path: "/catalog",
-        component: CatalogGame
-    },
-    {
-        path: "/store",
-        component: StoreGame
-    },
-    {
         path: "/",
         component: MainPage
+    },
+    {
+        path: "/rich",
+        component: RichNomination
+    },
+    {
+        path: "/builder",
+        component: BuilderNomination
+    },
+    {
+        path: "/king",
+        component: KingNomination
+    },
+    {
+        path: "/redstoner",
+        component: RedStonerNomination
+    },
+    {
+        path: "/creator",
+        component: CreatorNomination
+    },
+    {
+        path: "/event",
+        component: EventNomination
+    },
+    {
+        path: "/building",
+        component: BuildingNomination
     }
 ];
 
